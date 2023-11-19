0efccc16a59fdceb564f08c7b18485ee5096ca29
Yuto Takano <y.takano@sms.ed.ac.uk>
2023-11-19 00:33:07 +0000

    Implement generateRay in pinhole camera

--- src/cameras/camera.h
+++ src/cameras/camera.h
@@ -4,13 +4,29 @@
 #include "../vector.h"
 #include "../ray.h"
 
+/**
+ * @brief Generic camera model with a fixed film size.
+ */
 class Camera {
 public:
   Camera(Vector2f filmSize) : filmSize(filmSize) {};
   virtual ~Camera() = default;
 
   static std::unique_ptr<Camera> fromJson(const nlohmann::json &json);
+
+  /**
+   * @brief Generate a ray from the camera origin through the given film
+   * position, where the film position is in integer coordinates within the
+   * given filmSize for this class.
+   * @param filmPosition [0, filmSize.x()] x [0, filmSize.y()]
+   * @return Ray from the camera origin through the given film position
+   */
   virtual Ray generateRay(Point2f filmPosition) const = 0;
+
+  /**
+   * @brief Get the film size for this camera.
+   * @return Film size in pixels
+   */
   Vector2f getFilmSize() const;
 
 protected:
--- src/cameras/pinhole.cpp
+++ src/cameras/pinhole.cpp
@@ -7,7 +7,20 @@ Pinhole Pinhole::fromJson(const nlohmann::json &json) {
 }
 
 Ray Pinhole::generateRay(Point2f filmPosition) const {
-  Vector3f origin = position;
-  Vector3f direction = Vector3f(filmPosition.x(), filmPosition.y(), -1) - position;
-  return Ray(origin, direction.normalized());
+  // filmPosition is in [0, filmSize.x] x [0, filmSize.y]
+  float aspectRatio = filmSize.x() / filmSize.y();
+  float halfWidth = std::tan(fov / 2.0f);
+  float halfHeight = halfWidth / aspectRatio;
+
+  // w is the direction from camera origin to lookAt
+  Vector3f w = (position - lookAt).normalized();
+  // u is the direction from camera origin to the left of the image
+  Vector3f u = upVector.cross(w).normalized();
+  // v is the direction from camera origin to the top of the image, perpendicular to u and w
+  // This can be different from the upVector if the upVector is not perpendicular to w
+  Vector3f v = w.cross(u);
+
+  // direction is the direction from camera origin to the filmPosition
+  Vector3f direction = u * (filmPosition.x() / filmSize.x() - 0.5f) * 2.0f * halfWidth + v * (filmPosition.y() / filmSize.y() - 0.5f) * 2.0f * halfHeight - w;
+  return Ray(position, direction.normalized());
 }
