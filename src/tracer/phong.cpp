37f2c7bd7155e336a4e6d015a19d86a2562d961d
Yuto Takano <y.takano@sms.ed.ac.uk>
2023-11-26 17:05:20 +0000

    Implement reflection and refraction in phong tracer

--- src/tracer/phong.cpp
+++ src/tracer/phong.cpp
@@ -2,8 +2,9 @@
 #include "../ray.h"
 #include "../intersection.h"
 #include "../scene.h"
+#include <iostream>
 
-Phong::Phong(int bounces) : Tracer()
+Phong::Phong(int nbounces) : Tracer(), nbounces(nbounces)
 {
 }
 
@@ -15,12 +16,22 @@ Phong Phong::fromJson(const nlohmann::json &json_data)
   {
     nbounces = json_data["nbounces"];
   }
-
   return Phong(nbounces);
 }
 
 Color3f Phong::traceRay(const Scene scene, Ray ray, float minDepth, float maxDepth) const
 {
+  return traceRayRecursive(scene, ray, minDepth, maxDepth, 0);
+}
+
+Color3f Phong::traceRayRecursive(const Scene scene, Ray ray, float minDepth, float maxDepth, int bounce) const
+{
+  // Abort early if we've reached the maximum number of bounces already
+  if (bounce > nbounces)
+  {
+    return scene.getBackgroundColor();
+  }
+
   // Find closest intersection with an object in the scene
   std::optional<Intersection> intersection = scene.intersect(ray, minDepth, maxDepth);
 
@@ -74,6 +85,66 @@ Color3f Phong::traceRay(const Scene scene, Ray ray, float minDepth, float maxDep
     float distanceSquared = std::pow((light->position - intersection->point).length(), 2);
 
     color += (specular_color * std::pow(specular_intensity, specular_power) * ks + diffuse_color * diffuse_intensity * kd) * light->intensity / distanceSquared;
+  }
+
+  // Add refraction contribution
+  if (intersection->object->material->isRefractive && bounce < nbounces)
+  {
+    // Calculate the refracted ray direction, adapted from:
+    // https://paulbourke.net/geometry/reflected/
+    float n1 = 1.f;
+    float n2 = intersection->object->material->refractiveIndex;
+    Vector3f normal = intersection->normal;
+    if (ray.direction.dot(intersection->normal) > 0.f)
+    {
+      // Ray is from inside the object, so we need to flip the normal and swap
+      // the refractive indices
+      std::swap(n1, n2);
+      normal = intersection->normal * -1.f;
+    }
+    float n = n1 / n2;
+    float cosI = -normal.dot(ray.direction);
+    float sinT2 = n * n * (1.0 - cosI * cosI);
+    if (sinT2 > 1.0)
+    {
+      // Total internal reflection
+      return color;
+    }
+
+    float cosT = std::sqrt(1.0 - sinT2);
+    Vector3f refracted_ray_direction = ray.direction * n + normal * (n * cosI - cosT);
+
+    // Recursively find the refracted ray color until we reach the maximum
+    // number of bounces
+    Color3f refracted_ray_color = traceRayRecursive(scene, Ray(intersection->point - normal * 0.0001f, refracted_ray_direction), 0.f, 1000.f, bounce + 1);
+
+    // Add the refracted ray color to the current color
+    color = refracted_ray_color;
+  }
+
+  // Add reflection contribution
+  if (intersection->object->material->isReflective)
+  {
+    // Calculate the reflected ray direction, adapted from:
+    // https://paulbourke.net/geometry/reflected/
+    Vector3f reflected_ray_direction = ray.direction - intersection->normal * 2.f * ray.direction.dot(intersection->normal);
+
+    Vector3f normal = intersection->normal;
+    if (ray.direction.dot(intersection->normal) > 0.f)
+    {
+      // Ray is from inside the object, so we need to flip the normal and swap
+      // the refractive indices
+      normal = intersection->normal * -1.f;
+    }
+
+    // Recursively find the reflected ray color until we reach the maximum
+    // number of bounces
+    Color3f reflected_ray_color = traceRayRecursive(scene, Ray(intersection->point + intersection->normal * 0.0001f, reflected_ray_direction), 0.f, 1000.f, bounce + 1);
+
+    // We could add the reflected ray color to the current color, but this will
+    // result in very unrealistic bright reflectance surfaces. Instead, we
+    // combine the Blinn-Phong color and the refletance color with weights.
+    color = color * (1.0f - intersection->object->material->reflectivity) + reflected_ray_color * intersection->object->material->reflectivity;
   }
 
   return color;
--- src/tracer/phong.h
+++ src/tracer/phong.h
@@ -13,6 +13,20 @@ public:
   static Phong fromJson(const nlohmann::json &json_data);
 
   Color3f traceRay(const Scene scene, const Ray ray, float minDepth, float maxDepth) const override;
+
+private:
+
+  /**
+   * @brief Recursive helper function for traceRay, which adds the bounce arg.
+   * @param scene Scene object
+   * @param ray Camera ray
+   * @param minDepth Minimum ray length for intersection checks
+   * @param maxDepth Maximum ray length for intersection checks
+   * @param bounce Number of bounces so far
+  */
+  Color3f traceRayRecursive(const Scene scene, const Ray ray, float minDepth, float maxDepth, int bounce) const;
+
+  int nbounces;
 };
 
 #endif // PHONG_H
