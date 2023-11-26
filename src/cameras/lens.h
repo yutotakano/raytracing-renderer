eb93a4e7acd9966165651e08d3b7c78a020c57fe
Yuto Takano <y.takano@sms.ed.ac.uk>
2023-11-26 23:00:44 +0000

    Create temporary files for a lens-based PBR camera

---
+++ src/cameras/lens.h
@@ -0,0 +1,27 @@
+#ifndef LENS_H
+#define LENS_H
+
+#include "../vector.h"
+#include "camera.h"
+
+/**
+ * @brief Lens camera model for physically accurate renders.
+ */
+class Lens : public Camera {
+public:
+  Lens(Vector2f filmSize, Vector3f position, Vector3f lookAt, Vector3f upVector, float fov, float exposure);
+  static Lens fromJson(const nlohmann::json &json);
+
+  Ray generateRay(Point2f filmPosition) const;
+
+  float getExposure() const;
+
+private:
+  Vector3f position;
+  Vector3f lookAt;
+  Vector3f upVector;
+  float fov;
+  float exposure;
+};
+
+#endif // LENS_H
