8007cfb24ee3bc05d0dd52eadb668cda159c517a
Yuto Takano <y.takano@sms.ed.ac.uk>
2023-11-27 01:46:01 +0000

    Recognize the lens camera in JSON and parse it as such

--- src/cameras/camera.cpp
+++ src/cameras/camera.cpp
@@ -1,10 +1,13 @@
 #include "camera.h"
 #include "pinhole.h"
+#include "lens.h"
 
 std::unique_ptr<Camera> Camera::fromJson(const nlohmann::json &json) {
   std::string type = json["type"];
   if (type == "pinhole") {
     return std::make_unique<Pinhole>(Pinhole::fromJson(json));
+  } else if (type == "lens") {
+    return std::make_unique<Lens>(Lens::fromJson(json));
   } else {
     throw std::runtime_error("Unknown camera type: " + type);
   }
