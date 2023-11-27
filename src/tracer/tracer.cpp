37813b32b5d9a54ce3f6da3a5f594d6059fb4177
Yuto Takano <y.takano@sms.ed.ac.uk>
2023-11-27 01:45:42 +0000

    Recognize pathtracer in JSON and parse it as such

--- src/tracer/tracer.cpp
+++ src/tracer/tracer.cpp
@@ -1,6 +1,7 @@
 #include "tracer.h"
 #include "binary.h"
 #include "phong.h"
+#include "path.h"
 
 Tracer::Tracer(int sampleCount) : sampleCount(sampleCount)
 {
@@ -17,6 +18,10 @@ std::unique_ptr<Tracer> Tracer::fromJson(const nlohmann::json &json_data)
   else if (rendermode == "phong")
   {
     return std::make_unique<Phong>(Phong::fromJson(json_data));
+  }
+  else if (rendermode == "path")
+  {
+    return std::make_unique<Path>(Path::fromJson(json_data));
   }
   else
   {
